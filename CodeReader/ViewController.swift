64b412a26e6401eaa8fd39c82060d34fc8dce178
perkinsung <perkinsung@yvtcdeimac.local>
2016-12-01 13:42:32 +0800

    Initial Commit

---
+++ CodeReader/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CodeReader
+//
+//  Created by perkinsung on 2016/12/1.
+//  Copyright © 2016年 perkinsung. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
